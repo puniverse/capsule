ae1e2db2cab5496d791f380749040845d27804c5
pron <ron.pressler@gmail.com>
2014-04-21 14:33:25 +0300

    license headers

--- src/main/java/co/paralleluniverse/capsule/dependency/ConsoleTransferListener.java
+++ src/main/java/co/paralleluniverse/capsule/dependency/ConsoleTransferListener.java
@@ -3,7 +3,8 @@
  * Copyright (c) 2014, Parallel Universe Software Co. All rights reserved.
  * 
  * This program and the accompanying materials are licensed under the terms 
- * of the Eclipse Public License v1.0 as published by the Eclipse Foundation.
+ * of the Eclipse Public License v1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 /*
  * *****************************************************************************
