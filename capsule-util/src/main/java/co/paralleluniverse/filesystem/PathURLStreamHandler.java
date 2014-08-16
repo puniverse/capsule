f07848c326e394e0f701902add72d3fe9bd5ef83
pron <ron.pressler@gmail.com>
2014-08-16 16:35:15 +0300

    Support jimfs in URLClassLoader

---
+++ capsule-util/src/main/java/co/paralleluniverse/filesystem/PathURLStreamHandler.java
@@ -0,0 +1,25 @@
+package co.paralleluniverse.filesystem;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+final class PathURLStreamHandler extends URLStreamHandler {
+    static final URLStreamHandler INSTANCE = new PathURLStreamHandler();
+
+    @Override
+    protected URLConnection openConnection(URL url) throws IOException {
+        try {
+            URI uri = url.toURI();
+            Path path = Paths.get(uri);
+            return new PathURLConnection(url, path);
+        } catch (URISyntaxException e) {
+            throw new IOException("invalid URL", e);
+        }
+    }
+}
