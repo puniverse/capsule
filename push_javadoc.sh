bbbf8b43195cddc90acec3e2c597409672c88434
pron <ron.pressler@gmail.com>
2014-08-23 15:35:56 +0300

    push_javadoc [ci skip]

---
+++ push_javadoc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Push javadoc files to a website hosted by github <http://pages.github.com/>.
+# Before executing this script, generate the javadoc files into build/docs/javadoc/.
+git checkout gh-pages || exit $?
+
+# Clear out the old files:
+rm -rf capsule/javadoc
+rm -rf capsule-util/javadoc
+rm -rf capsule-container/javadoc
+
+# Replace them with new files and commit them:
+cp -pr capsule/build/docs/javadoc/ capsule/javadoc
+cp -pr capsule-util/build/docs/javadoc/ capsule-util/javadoc
+cp -pr capsule-container/build/docs/javadoc/ capsule-container/javadoc
+
+git add -A \
+&& git commit -a -m "generated javadoc"
+
+ERROR=$?
+
+git checkout master || exit $?
+[ $ERROR -eq 0 ] || exit $ERROR
+git push origin gh-pages || exit $?
