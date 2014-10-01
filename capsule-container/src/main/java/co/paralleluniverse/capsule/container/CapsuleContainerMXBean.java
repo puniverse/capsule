a0b1eca23b08d05586dfe640cf66c9082c6f600d
pron <ron.pressler@gmail.com>
2014-10-01 21:44:34 +0300

    Copyright notice [ci skip]

--- capsule-container/src/main/java/co/paralleluniverse/capsule/container/CapsuleContainerMXBean.java
+++ capsule-container/src/main/java/co/paralleluniverse/capsule/container/CapsuleContainerMXBean.java
@@ -1,12 +1,11 @@
 /*
  * Capsule
- * Copyright (c) 2014, Parallel Universe Software Co. All rights reserved.
+ * Copyright (c) 2014, Parallel Universe Software Co. and Contributors. All rights reserved.
  * 
  * This program and the accompanying materials are licensed under the terms 
  * of the Eclipse Public License v1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-
 package co.paralleluniverse.capsule.container;
 
 import java.util.Set;
