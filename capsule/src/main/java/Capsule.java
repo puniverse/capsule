bac475383f399d4b2257a5b4a91ef400e2413172
pron <ron.pressler@gmail.com>
2014-11-12 13:54:14 +0200

    Close #44

--- capsule/src/main/java/Capsule.java
+++ capsule/src/main/java/Capsule.java
@@ -3028,21 +3028,21 @@ public class Capsule implements Runnable {
      */
     protected String expand(String str) {
         if ("$0".equals(str))
-            return jarFile.toString();
+            return processOutgoingPath(jarFile);
 
         if (appCache != null)
-            str = str.replaceAll("\\$" + VAR_CAPSULE_DIR, appCache.toAbsolutePath().toString());
+            str = str.replace("$" + VAR_CAPSULE_DIR, processOutgoingPath(appCache));
         else if (str.contains("$" + VAR_CAPSULE_DIR))
             throw new IllegalStateException("The $" + VAR_CAPSULE_DIR + " variable cannot be expanded when the capsule is not extracted");
 
         str = expandCommandLinePath(str);
-        if (appId == null && str.contains("\\$" + VAR_CAPSULE_APP))
+        if (appId == null && str.contains("$" + VAR_CAPSULE_APP))
             throw new IllegalStateException("Cannot use $" + VAR_CAPSULE_APP + " variable in an empty capsule. (in: " + str + ")");
-        str = str.replaceAll("\\$" + VAR_CAPSULE_APP, appId);
-        str = str.replaceAll("\\$" + VAR_CAPSULE_JAR, jarFile.toString());
-        final String jhome = toString(getJavaHome());
+        str = str.replace("$" + VAR_CAPSULE_APP, appId);
+        str = str.replace("$" + VAR_CAPSULE_JAR, processOutgoingPath(jarFile));
+        final String jhome = processOutgoingPath(getJavaHome());
         if (jhome != null)
-            str = str.replaceAll("\\$" + VAR_JAVA_HOME, jhome);
+            str = str.replace("$" + VAR_JAVA_HOME, jhome);
         str = str.replace('/', FILE_SEPARATOR_CHAR);
         return str;
     }
