2c2882ef046dab961bb34a969c8e3fcc3077d16b
pron <ron.pressler@gmail.com>
2014-11-28 03:46:57 +0200

    Add Icon attribute

--- capsule/src/main/java/Capsule.java
+++ capsule/src/main/java/Capsule.java
@@ -161,6 +161,7 @@ public class Capsule implements Runnable {
     private static final String ATTR_NATIVE_DEPENDENCIES_WIN = registerAttribute("Native-Dependencies-Win", null, true, null, "A list of Maven dependencies consisting of `.dll` artifacts for Linux; each item can be a comma separated pair, with the second component being a new name to give the download artifact");
     private static final String ATTR_NATIVE_DEPENDENCIES_MAC = registerAttribute("Native-Dependencies-Mac", null, true, null, "A list of Maven dependencies consisting of `.dylib` artifacts for Mac OS X; each item can be a comma separated pair, with the second component being a new name to give the download artifact");
     private static final String ATTR_LOG_LEVEL = registerAttribute("Capsule-Log-Level", null, false, PROP_LOG_LEVEL, "The capsule's default log level");
+    private static final String ATTR_ICON = registerAttribute("Icon", null, true, null, "The name of the application's icon file(s), with no suffix, relative to the capsule root");
 
     // outgoing
     private static final String VAR_CAPSULE_APP = "CAPSULE_APP";
