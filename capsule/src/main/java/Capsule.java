d366e5cd4eed903c8c1bf2b02309249bbdc753af
pron <ron.pressler@gmail.com>
2015-04-08 16:35:32 +0300

    -

--- capsule/src/main/java/Capsule.java
+++ capsule/src/main/java/Capsule.java
@@ -1148,11 +1148,13 @@ public class Capsule implements Runnable {
 
     //<editor-fold defaultstate="collapsed" desc="Threads">
     /////////// Threads ///////////////////////////////////
-    private void startThread(String name, String method) {
+    private Thread startThread(String name, String method) {
         if (threads.containsKey(name))
             throw new IllegalStateException("A thread by the name " + name + " has already been registered.");
         threads.put(name, method);
-        new Thread(this, name).start();
+        final Thread t = new Thread(this, name);
+        t.start();
+        return t;
     }
 
     /**
