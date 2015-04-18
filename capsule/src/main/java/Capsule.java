31426d00274df0ea88ca93be835b1be744b380f2
pron <ron.pressler@gmail.com>
2015-04-18 03:01:28 +0300

    -

--- capsule/src/main/java/Capsule.java
+++ capsule/src/main/java/Capsule.java
@@ -44,7 +44,6 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
