e83408b41d8e865c2e8bfc6e40baa779618c85a3
pron <ron.pressler@gmail.com>
2016-12-19 21:01:23 +0200

    -

--- capsule/src/main/java/Capsule.java
+++ capsule/src/main/java/Capsule.java
@@ -133,7 +133,7 @@ import static java.util.Arrays.asList;
  * @author pron
  */
 public class Capsule implements Runnable, InvocationHandler {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.4";
     /*
      * This class follows some STRICT RULES:
      *
